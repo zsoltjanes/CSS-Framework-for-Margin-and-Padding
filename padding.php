53658490f2b6dfe8dc3c74dcd884e3858683b673
Chamekh <chamekhala@gmail.com>
2022-07-31 21:03:12 +0100

    Optimize padding ;)

--- padding.php
+++ padding.php
@@ -1,39 +1,16 @@
 <?php
 
-for ($i = 0; $i <= 1000; $i++) {
-    echo '.pr' . $i . '{padding-right:' . $i . 'px;}';
-}
+    for ($i = 0; $i <= 1000; $i++) {
+        echo '.pr' . $i . '{padding-right:' . $i . 'px;}';
+        echo '.pl' . $i . '{padding-left:' . $i . 'px;}';
+        echo '.pt' . $i . '{padding-top:' . $i . 'px;}';
+        echo '.pb' . $i . '{padding-bottom:' . $i . 'px;}';
 
-for ($i = 0; $i <= 1000; $i++) {
-    echo '.pl' . $i . '{padding-left:' . $i . 'px;}';
-}
-
-for ($i = 0; $i <= 1000; $i++) {
-    echo '.pt' . $i . '{padding-top:' . $i . 'px;}';
-}
-
-for ($i = 0; $i <= 1000; $i++) {
-    echo '.pb' . $i . '{padding-bottom:' . $i . 'px;}';
-}
-
-
-
-
-
-
-for ($i = 1; $i <= 1000; $i++) {
-    echo '.prm' . $i . '{padding-right:-' . $i . 'px;}';
-}
-
-for ($i = 1; $i <= 1000; $i++) {
-    echo '.plm' . $i . '{padding-left:-' . $i . 'px;}';
-}
-
-for ($i = 1; $i <= 1000; $i++) {
-    echo '.ptm' . $i . '{padding-top:-' . $i . 'px;}';
-}
-
-for ($i = 1; $i <= 1000; $i++) {
-    echo '.pbm' . $i . '{padding-bottom:-' . $i . 'px;}';
-}
+        if ($i > 0) { 
+            echo '.prm' . $i . '{padding-right:' . $i . 'px;}';
+            echo '.plm' . $i . '{padding-left:' . $i . 'px;}';
+            echo '.ptm' . $i . '{padding-top:' . $i . 'px;}';
+            echo '.pbm' . $i . '{padding-bottom:' . $i . 'px;}';
+        }
+    } 
 ?>

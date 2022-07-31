ed73342ea1679efd50aa3126e5aff0251bc5796a
Chamekh <chamekhala@gmail.com>
2022-07-31 21:05:32 +0100

    Optimize margin

--- margin.php
+++ margin.php
@@ -1,39 +1,16 @@
 <?php
 
-for ($i = 0; $i <= 1000; $i++) {
-    echo '.mr' . $i . '{margin-right:' . $i . 'px;}';
-}
+    for ($i = 0; $i <= 1000; $i++) {
+        echo '.mr' . $i . '{margin-right:' . $i . 'px;}';
+        echo '.ml' . $i . '{margin-left:' . $i . 'px;}';
+        echo '.mt' . $i . '{margin-top:' . $i . 'px;}';
+        echo '.mb' . $i . '{margin-bottom:' . $i . 'px;}';
 
-for ($i = 0; $i <= 1000; $i++) {
-    echo '.ml' . $i . '{margin-left:' . $i . 'px;}';
-}
-
-for ($i = 0; $i <= 1000; $i++) {
-    echo '.mt' . $i . '{margin-top:' . $i . 'px;}';
-}
-
-for ($i = 0; $i <= 1000; $i++) {
-    echo '.mb' . $i . '{margin-bottom:' . $i . 'px;}';
-}
-
-
-
-
-
-
-for ($i = 1; $i <= 1000; $i++) {
-    echo '.mrm' . $i . '{margin-right:-' . $i . 'px;}';
-}
-
-for ($i = 1; $i <= 1000; $i++) {
-    echo '.mlm' . $i . '{margin-left:-' . $i . 'px;}';
-}
-
-for ($i = 1; $i <= 1000; $i++) {
-    echo '.mtm' . $i . '{margin-top:-' . $i . 'px;}';
-}
-
-for ($i = 1; $i <= 1000; $i++) {
-    echo '.mbm'. $i . '{margin-bottom:-' . $i . 'px;}';
-}
+        if ($i > 0) { 
+            echo '.mrm' . $i . '{margin-right:' . $i . 'px;}';
+            echo '.mlm' . $i . '{margin-left:' . $i . 'px;}';
+            echo '.mtm' . $i . '{margin-top:' . $i . 'px;}';
+            echo '.mbm' . $i . '{margin-bottom:' . $i . 'px;}';
+        }
+    } 
 ?>
